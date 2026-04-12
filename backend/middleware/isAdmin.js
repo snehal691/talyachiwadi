a4a92784b30030d0011c9de5a712e1226f3d9606
Kiran Raut <kiranraut04196@gmail.com>
2026-04-12 11:26:03 +0530

    refact: improve isAdmin middleware by adding jwt verification

--- backend/middleware/isAdmin.js
+++ backend/middleware/isAdmin.js
@@ -1,22 +1,35 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import jwt from "jsonwebtoken"
 
 
 export const isAdmin = asyncHandler(async (req, res, next) => {
     //cookie mdhe ahe jwt
-    const admin = req.admin
-    const role = admin?.role
+    const admin = req?.admin
+    let newAdmin;
+
+    if (!admin) {
+        const accessToken = req?.cookies?.accessToken
+
+        try {
+            newAdmin = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+
+        } catch (error) {
+            throw new ApiError(401, "Unauthorized: Access token missing");
+        }
+    }
+    const role = admin?.role || newAdmin?.role
 
     const allowedRoles = ["admin", "developer"]
 
-    if(!role){
+    if (!role) {
         throw new ApiError(401, "Unauthorized: Access token missing");
     }
-    if(!allowedRoles.includes(role)){
+
+    if (!allowedRoles.includes(role)) {
         throw new ApiError(401, "Unauthorized: You are not an admin");
     }
-    
+
     next();
 
 })
